ccfc64e5527ce4cd7d509402f196d562eecf72d5
Ari <anna-lotta.kaukes@khk.ee>
2017-04-20 16:02:31 +0300

    Added main.html code + css and some bugs were squashed

--- index.php
+++ index.php
@@ -8,15 +8,21 @@
 // create and template object
 define('CLASSES_DIR', 'classes/');
 define('TMPL_DIR', 'tmpl/');
+define('STYLE_DIR', 'css/');
 require_once TMPL_DIR.'template.php';
 // and use it
 // create a template object
 // set up the file name for template
 // load template file content
-$tmpl = new template('main.php');
+$tmpl = new template('main.html');
+// add pairs of template element names and real values
+$tmpl->set('menu', 'Menu');
+$tmpl->set('nav_bar', 'Navigation');
+$tmpl->set('lang_bar', 'Languages');
+$tmpl->set('content', 'Content');
+$tmpl->set('style', STYLE_DIR.'main.css');
+$tmpl->set('header', 'Page title');
 // control the content of template object
-echo '<pre>';
-print_r($tmpl);
-echo '</pre>';
+echo $tmpl->parse();
 
 ?>
