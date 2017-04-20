429a440de36a100d244d8ff009d8b4c69aec6729
Ari <anna-lotta.kaukes@khk.ee>
2017-04-20 13:51:56 +0300

    Testing text object

--- test.php
+++ test.php
@@ -6,6 +6,21 @@
  * Time: 1:45 PM
  */
 
-echo 'php stormi test';
+// require the object creating and using class
+require_once('OOP/text.php');
+// create an object
+$sentence = new text();
+// control object output
+echo '<pre>';
+print_r($sentence);
+echo '</pre>';
+//set text
+$sentence->setText('Text object');
+//control object output
+echo '<pre>';
+print_r($sentence);
+echo '</pre>';
+// show object output
+$sentence->show();
 
 ?>
