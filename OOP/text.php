a9587335594be92ffb891677558c6872a83884d6
Ari <anna-lotta.kaukes@khk.ee>
2017-04-20 13:45:12 +0300

    Text class OOP

---
+++ OOP/text.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Ari
+ * Date: 20-Apr-17
+ * Time: 1:43 PM
+ */
+class text
+{
+    // class text begin
+    // class variables = instance variables
+
+    var $str = '';
+
+    //methods
+    // set text function
+
+    function setText($s)
+    {
+        $this->str = $s;
+    }//setText
+
+    // show text function
+    function show()
+    {
+        echo $this->str . '<br/>';
+    }//show
+}//text class end
+?>
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ari
+ * Date: 20-Apr-17
+ * Time: 1:40 PM
+ */
+
