8e242feb19f21a1d204e292af228e63ed3dcd719
Ari <anna-lotta.kaukes@khk.ee>
2017-05-02 15:06:54 +0300

    last commit

--- menu.php
+++ menu.php
@@ -6,36 +6,37 @@
  * Time: 3:21 PM
  */
 
-$menu = new template('menu.menu'); // in menu directory is file menu.html menu/menu.html
-$item = new template('menu.item');
-// menu item creation - begin
-// add pairs of item template element names and real values
-$item->set('name', 'First page');
-$link = $http->getLink(array('act'=>'first'));
-$item->set('link',$link);
-// control created item output
-//echo '<pre>';
-//print_r($item);
-//echo '</pre>'
-//add menu item to menu
-$menu->set('items',$item->parse());
-// menu item creation - end
-//
-// menu item creation - begin
-// add pairs of item template element name and real values
-$item->set('name', 'Second page');
-$link = $http->getLink(array('act'=>'second'));
-$item->set('link', $link);
-// control created item output
-//echo '<pre>';
-//print_r($item);
-//echo '</pre>'
-// add menu item to menu
-$menu->add('items', $item->parse()); // add another item to menu
-// menu item creation - end
-//
-// control created menu output
-//echo'<pre>';
-//print_r($menu);
-//echo'</pre>';
+$menu = new Template('menu.menu');
+$menu->set('items', false); // if not logged in, cant see the menu
+$item = new Template('menu.item');
+
+// show menu
+$sql = 'SELECT content_id, title FROM content WHERE '.
+    'parent_id="0" AND '.
+    'show_in_menu="1"';
+// if not an admin can only see some contenct, when hidden = 1 - is available only for admins
+if(ROLE_ID != ROLE_ADMIN) {
+    $sql .= ' and is_hidden="0"';
+}
+$sql .= ' ORDER BY sort ASC';
+$res = $db->getArray($sql);
+
+if($res != false){
+    foreach ($res as $page) {
+        $link = $http->getLink(array('page_id' => $page['content_id']));
+        $item->set('link', $link);
+        $item->set('name', $page['title']);
+        $menu->add('items', $item->parse());
+    }
+}
+// if the user is logged in - either admin or normal user
+// we create them a way to log out
+if(USER_ID != ROLE_NONE){
+    $link = $http->getLink(array('act' => 'logout'));
+    $item->set('link', $link);
+    $item->set('name', 'Log out');
+    $menu->add('items', $item->parse());
+}
+$tmpl->set('menu', $menu->parse());
+
 ?>
