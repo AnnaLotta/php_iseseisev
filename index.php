133c13d8f512baaec4419f5c7f756fad9a1e0184
Ari <anna-lotta.kaukes@khk.ee>
2017-05-02 13:40:53 +0300

    changes in index.php

--- index.php
+++ index.php
@@ -5,11 +5,12 @@
  * Date: 20-Apr-17
  * Time: 1:40 PM
  */
+require_once 'conf.php';
 // create and template object
-define('CLASSES_DIR', 'classes/');
-define('TMPL_DIR', 'tmpl/');
-define('STYLE_DIR', 'css/');
-require_once TMPL_DIR . 'template.php';
+// define('CLASSES_DIR', 'classes/');
+// define ('TMPL_DIR', 'tmpl/');
+// define('STYLE_DIR', 'css/');
+require_once CLASSES_DIR.'template.php';
 // and use it
 // create a template object
 // set up the file name for template
@@ -25,4 +26,6 @@ $tmpl->set('header', 'Page title');
 // control the content of template object
 echo $tmpl->parse();
 
+// echo '<pre>;
+//print_r($db);
 ?>
